3d5d60b1fce6186c36a2bb7f0b01a2169f155e5f
Justin Bozonier <darkxanthos@gmail.com>
2010-02-01 19:43:06 -0800

    Implemented an IEnumerable message channel.

--- StarbucksExample/StarbucksExample/MessagingSystem/ThreadBlockingChannel.cs
+++ StarbucksExample/StarbucksExample/MessagingSystem/ThreadBlockingChannel.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Threading;
-using StarbucksExample.Messages;
 
 namespace StarbucksExample.MessagingSystem
 {
@@ -35,58 +34,6 @@ namespace StarbucksExample.MessagingSystem
                 return result;
             }
 
-        }
-    }
-
-    public class EnumerableChannel : IEnqueue, IEnumerable, IEnumerator
-    {
-        private Queue q = Queue.Synchronized(new Queue());
-        private readonly ManualResetEvent newItemEntered = new ManualResetEvent(false);
-
-        public void Enqueue(object o)
-        {
-            lock (this)
-            {
-                q.Enqueue(o);
-                newItemEntered.Set();
-            }
-        }
-
-        object _Dequeue()
-        {
-            newItemEntered.WaitOne();
-
-            lock (this)
-            {
-                var result = q.Dequeue();
-                if (q.Count == 0)
-                    newItemEntered.Reset();
-
-                return result;
-            }
-
-        }
-
-        public IEnumerator GetEnumerator()
-        {
-            return this;
-        }
-
-        public bool MoveNext()
-        {
-            Current = _Dequeue();
-
-            return !(Current is TerminateProcessMessage);
-        }
-
-        public void Reset()
-        {
-
-        }
-
-        public object Current
-        {
-            get; private set;
         }
     }
 }
